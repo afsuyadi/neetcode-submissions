6e2720152bbf8c6b3e0892caaa719159ed3924f4
Suyadi <104284194+afsuyadi@users.noreply.github.com>
2026-07-27 19:10:26 +0700

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.go
@@ -0,0 +1,32 @@
+func topKFrequent(nums []int, k int) []int {
+	// map setiap elemen di dalam nums terhadap freq nya
+	count := make(map[int]int)
+	for _, num := range nums {
+		count[num]++
+	}
+	// output = {
+		// 1: 1,
+		// 2: 2,
+		// 3: 3,	
+	//}
+	// reorder element memakai freq sebagai index nya 
+	buckets := make([][]int, len(nums)+1) // eg. [[],[],[],[],[],[],[]]
+	for num, freq := range count { // eg. 3:3
+		buckets[freq] = append(buckets[freq], num) // eg. [[],[],[],[3],[],[],[]]
+	}
+	// output= [[],[1],[2],[3]],[],[],[]]
+	// mulai dari belakang
+	result := []int{} 
+	for freq := len(buckets) - 1; freq > 0; freq-- {
+		for _, num := range buckets[freq] {
+			result = append(result, num)
+			if len(result) == k {
+				return result
+			}
+		}
+			
+	}
+	return []int{}
+	// time: O(n)
+	// space: O(n)
+}
